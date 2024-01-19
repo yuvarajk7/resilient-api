f94692cf256e5adf480e716a34dc369cc73cfefc
Yuvaraj <yuvaraj.kesavan@hotmail.com>
2024-01-19 12:12:58 -0600

    added endpoint and route contraints

--- resilientapi/Program.cs
+++ resilientapi/Program.cs
@@ -24,6 +24,26 @@ app.UseSwagger();
 app.UseSwaggerUI( c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 
 app.MapGet("/", () => "Hello World!");
+
 app.MapGet("/{name}", ([FromRoute] string name, IHelloService service) => service.Hello(name));
+
+app.MapGet("/date/{date}", (DateTime date) => date.ToString());
+
+app.MapGet("/uniqueidentifier/{id}", (Guid id) => id.ToString());
+
+app.MapMethods("/users/{userId}", new List<string> { "PUT", "PATCH" }, (HttpRequest request) =>
+{
+    var id = request.RouteValues["id"];
+    var lastActivityDate = request.Form["lastactivitydate"];
+});
+
+app.MapMethods("/routeName", new List<string> { "OPTIONS", "HEAD", "TRACE" }, () => { 
+    // Do action
+});
+
+
+app.MapGet("/provinces/{provinceId:int:max(12)}", (int provinceId) => $"ProvinceId {provinceId}");
+
+
 
 app.Run();
